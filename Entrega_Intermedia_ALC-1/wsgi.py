8e623870643f107ab0a3444e6400b7b13cb710ee
Alexander La Cruz <mvlctrading@gmail.com>
2022-12-15 00:14:38 -0500

    Minor Updates

--- Entrega_Intermedia_ALC-1/wsgi.py
+++ Entrega_Intermedia_ALC-1/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for proyecto34640 project.
+WSGI config for Entrega_Intermedia_ALC-1 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "proyecto34640.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Entrega_Intermedia_ALC-1")
 
 application = get_wsgi_application()
